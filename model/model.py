590b3555c4e568627bb55f276381fe2af583ef94
Mohamed Moursi <mahmedgamal995@gmail.com>
2021-03-02 02:44:48 +0100

    crossEntropy Loss function and more config

--- model/model.py
+++ model/model.py
@@ -31,28 +31,10 @@ class VGG_net(nn.Module):
         layers = []
         in_channels = self.in_channels 
         for layer in arch:
-            if layer == 'm':
+            if layer == 'M':
                 layers.append(nn.MaxPool2d(kernel_size=(2,2),stride=(2,2)))
             else:
                 out_channels = layer
-                layers+=[nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=(3,3),stride=(1,1),padding=(1,1)),nn.BatchNorm2d(layer),nn.ReLU()]
+                layers+=[nn.Conv2d(in_channels=in_channels,out_channels=out_channels,kernel_size=3,stride=1,padding=1),nn.BatchNorm2d(layer),nn.ReLU()]
                 in_channels=layer
         return nn.Sequential(*layers)
-
-# class MnistModel(BaseModel):
-#     def __init__(self, num_classes=10):
-#         super().__init__()
-#         self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-#         self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-#         self.conv2_drop = nn.Dropout2d()
-#         self.fc1 = nn.Linear(320, 50)
-#         self.fc2 = nn.Linear(50, num_classes)
-
-#     def forward(self, x):
-#         x = F.relu(F.max_pool2d(self.conv1(x), 2))
-#         x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-#         x = x.view(-1, 320)
-#         x = F.relu(self.fc1(x))
-#         x = F.dropout(x, training=self.training)
-#         x = self.fc2(x)
-#         return F.log_softmax(x, dim=1)
