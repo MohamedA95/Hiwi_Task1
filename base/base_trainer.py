07c8797175f6925ac3c7094f0007ed8317733153
Mohamed Moursi <mahmedgamal995@gmail.com>
2021-03-13 22:03:43 +0100

    fix save best bug :)

--- base/base_trainer.py
+++ base/base_trainer.py
@@ -89,6 +89,7 @@ class BaseTrainer:
                     best = True
                 else:
                     not_improved_count += 1
+                    self.logger.info("Validation performance did\'t improve for epoch#: {}".format(epoch))
 
                 if not_improved_count > self.early_stop:
                     self.logger.info("Validation performance didn\'t improve for {} epochs. "
