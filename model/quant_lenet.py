8906583f502da1425209fe64fe9fc389d8ead1df
mohamedA95 <mahmedgamal995@gmail.com>
2021-12-22 15:12:22 +0100

    Added options from common.py & used functions to define the layers.

--- model/quant_lenet.py
+++ model/quant_lenet.py
@@ -1,35 +1,60 @@
-#Based on brevitas examples
+# Based on brevitas examples
 from torch import nn
 from torch.nn import Module
 import torch.nn.functional as F
+import brevitas
 import brevitas.nn as qnn
-from brevitas.quant import Int8ActPerTensorFixedPoint
+from brevitas.core.scaling import ScalingImplType
+from brevitas.core.quant import QuantType
+from brevitas.core.stats import StatsOp
+from brevitas.core.restrict_val import RestrictValueType
 from brevitas.quant import Int8ActPerTensorFixedPoint, Int8ActPerTensorFloat
 from brevitas.quant import Int8WeightPerTensorFixedPoint, Int8WeightPerTensorFloat
 from brevitas.quant import Int8Bias, Int8BiasPerTensorFloatInternalScaling, Int8BiasPerTensorFixedPointInternalScaling, Int16Bias, IntBias
 
-BIAS_QUANTIZER = Int8Bias
-WEIGHT_QUANTIZER = Int8WeightPerTensorFixedPoint
+QUANT_TYPE = QuantType.INT
+SCALING_MIN_VAL = 2e-16
+
+ACT_SCALING_IMPL_TYPE = ScalingImplType.PARAMETER
+ACT_SCALING_PER_CHANNEL = True # custom 200 epoch
+ACT_SCALING_RESTRICT_SCALING_TYPE = RestrictValueType.LOG_FP
+ACT_MAX_VAL = 6.0
 ACT_QUANTIZER = Int8ActPerTensorFixedPoint
+ACT_PER_CHANNEL_BROADCASTABLE_SHAPE = None
+
+WEIGHT_SCALING_IMPL_TYPE = ScalingImplType.STATS
+WEIGHT_SCALING_PER_OUTPUT_CHANNEL = True # custom 200 epoch
+WEIGHT_SCALING_STATS_OP = StatsOp.MAX
+WEIGHT_QUANTIZER = Int8WeightPerTensorFixedPoint
+WEIGHT_RESTRICT_SCALING_TYPE = RestrictValueType.LOG_FP
+WEIGHT_NARROW_RANGE = False # custom 200 epoch
+
+INPUT_SCALING_PER_OUTPUT_CHANNEL = True
+
+ACT_RETURN_QUANT_TENSOR = True 
+CONV_RETURN_QUANT_TENSOR = True
+LINEAR_RETURN_QUANT_TENSOR = True
+ENABLE_BIAS_QUANT = True # enable bias quantization, default: False
+BIAS_QUANTIZER = Int8Bias
+
 class QuantLeNet(Module):
-    def __init__(self,bit_width=3):
+    def __init__(self, bit_width=8):
         super(QuantLeNet, self).__init__()
         self.features = nn.Sequential(
-            qnn.QuantIdentity(bit_width=bit_width, act_quant=ACT_QUANTIZER, return_quant_tensor=True),
-            make_conv(bit_width,3,32,3),
-            qnn.QuantReLU(return_quant_tensor=True,output_quant=ACT_QUANTIZER),
-            qnn.QuantMaxPool2d(kernel_size=2, stride=2,return_quant_tensor=True),
-            make_conv(bit_width,32,64,3),
-            qnn.QuantReLU(return_quant_tensor=True,output_quant=ACT_QUANTIZER),
-            qnn.QuantMaxPool2d(kernel_size=2, stride=2,return_quant_tensor=True))
+            make_quant_conv2d(3, 32, 3,bit_width,bias=True),
+            make_quant_relu(bit_width=bit_width),
+            qnn.QuantMaxPool2d(kernel_size=2, stride=2, return_quant_tensor=True),
+            make_quant_conv2d(32, 64, 3,bit_width,bias=True,input_quant=None),
+            make_quant_relu(bit_width=bit_width),
+            qnn.QuantMaxPool2d(kernel_size=2, stride=2, return_quant_tensor=True))
         self.classifier = nn.Sequential(
-            qnn.QuantLinear(64*6*6, 120, bias=True, weight_bit_width=bit_width,return_quant_tensor=True, bias_quant=BIAS_QUANTIZER, weight_quant=WEIGHT_QUANTIZER,output_quant=ACT_QUANTIZER),
-            qnn.QuantReLU(return_quant_tensor=True,output_quant=ACT_QUANTIZER),
-            qnn.QuantLinear(120, 84, bias=True, weight_bit_width=bit_width,return_quant_tensor=True, bias_quant=BIAS_QUANTIZER, weight_quant=WEIGHT_QUANTIZER,output_quant=ACT_QUANTIZER),
-            qnn.QuantReLU(return_quant_tensor=True,output_quant=ACT_QUANTIZER),
-            qnn.QuantLinear(84, 10, bias=False, weight_bit_width=bit_width,return_quant_tensor=False))
-        self.features[1].cache_inference_quant_bias=True
-        self.features[4].cache_inference_quant_bias=True
+            make_quant_linear(in_channels=64*6*6,out_channels=120,bit_width=bit_width,bias=True),
+            make_quant_relu(bit_width=bit_width),
+            make_quant_linear(in_channels=120,out_channels=84,bit_width=bit_width,bias=True),
+            make_quant_relu(bit_width=bit_width),
+            make_quant_linear(in_channels=84,out_channels=10,bit_width=bit_width,bias=True,return_quant_tensor=False))
+        self.features[1].cache_inference_quant_bias = True
+        self.features[4].cache_inference_quant_bias = True
         self.classifier[0].cache_inference_quant_bias = True
         self.classifier[2].cache_inference_quant_bias = True
         self.classifier[4].cache_inference_quant_bias = True
@@ -40,12 +65,116 @@ class QuantLeNet(Module):
         out = self.classifier(out)
         return out
 
-def make_conv(bit_width,in_channels,out_channels,kernel):
-    return qnn.QuantConv2d(in_channels=in_channels,
-                            out_channels=out_channels,
-                            kernel_size= kernel,
-                            weight_bit_width=bit_width,
-                            return_quant_tensor=True,
-                            bias_quant=BIAS_QUANTIZER,
-                            weight_quant=WEIGHT_QUANTIZER,
-                            output_quant=ACT_QUANTIZER)
+
+def make_quant_conv2d(in_channels,
+                      out_channels,
+                      kernel_size,
+                      bit_width,
+                      stride=1,
+                      padding=0,
+                      groups=1,
+                      bias=True,
+                      weight_quant=WEIGHT_QUANTIZER,
+                      bias_quant=BIAS_QUANTIZER,
+                      input_quant=ACT_QUANTIZER,
+                      return_quant_tensor=CONV_RETURN_QUANT_TENSOR, #Custom
+                      enable_bias_quant=ENABLE_BIAS_QUANT,
+                      weight_quant_type=QUANT_TYPE,
+                      input_scaling_per_output_channel=INPUT_SCALING_PER_OUTPUT_CHANNEL,
+                      weight_scaling_impl_type=WEIGHT_SCALING_IMPL_TYPE,
+                      weight_scaling_stats_op=WEIGHT_SCALING_STATS_OP,
+                      weight_scaling_per_output_channel=WEIGHT_SCALING_PER_OUTPUT_CHANNEL,
+                      weight_restrict_scaling_type=WEIGHT_RESTRICT_SCALING_TYPE,
+                      weight_narrow_range=WEIGHT_NARROW_RANGE,
+                      weight_scaling_min_val=SCALING_MIN_VAL):
+    bias_quant_type = QUANT_TYPE if enable_bias_quant else QuantType.FP
+    return qnn.QuantConv2d(in_channels,
+                           out_channels,
+                           groups=groups,
+                           kernel_size=kernel_size,
+                           padding=padding,
+                           stride=stride,
+                           bias=bias,
+                           weight_quant=weight_quant,
+                           bias_quant=bias_quant,
+                           input_quant=input_quant,
+                           input_bit_width=bit_width,
+                           return_quant_tensor=return_quant_tensor,
+                           output_quant=ACT_QUANTIZER,
+                           output_bit_width=bit_width,
+                        #    input_scaling_per_output_channel=input_scaling_per_output_channel,
+                        #    input_scaling_stats_permute_dims=(1, 0, 2), # permute dims to put channels first during stats collection phase
+                        #    input_per_channel_broadcastable_shape=(1, CHANNELS, 1), # shape of the learned parameter
+                           bias_quant_type=bias_quant_type,
+                           compute_output_bit_width=bias and enable_bias_quant,
+                           compute_output_scale=bias and enable_bias_quant,
+                           weight_bit_width=bit_width,
+                           weight_quant_type=weight_quant_type,
+                           weight_scaling_impl_type=weight_scaling_impl_type,
+                           weight_scaling_stats_op=weight_scaling_stats_op,
+                           weight_scaling_per_output_channel=weight_scaling_per_output_channel,
+                           weight_restrict_scaling_type=weight_restrict_scaling_type,
+                           weight_narrow_range=weight_narrow_range,
+                           weight_scaling_min_val=weight_scaling_min_val
+                           )
+
+
+def make_quant_linear(in_channels,
+                      out_channels,
+                      bit_width,
+                      bias,
+                      weight_quant=WEIGHT_QUANTIZER,
+                      bias_quant=BIAS_QUANTIZER,
+                      return_quant_tensor=LINEAR_RETURN_QUANT_TENSOR,
+                      output_quant=ACT_QUANTIZER,
+                      enable_bias_quant=ENABLE_BIAS_QUANT,
+                      weight_quant_type=QUANT_TYPE,
+                      weight_scaling_impl_type=WEIGHT_SCALING_IMPL_TYPE,
+                      weight_scaling_stats_op=WEIGHT_SCALING_STATS_OP,
+                      weight_scaling_per_output_channel=WEIGHT_SCALING_PER_OUTPUT_CHANNEL,
+                      weight_restrict_scaling_type=WEIGHT_RESTRICT_SCALING_TYPE,
+                      weight_narrow_range=WEIGHT_NARROW_RANGE,
+                      weight_scaling_min_val=SCALING_MIN_VAL):
+    bias_quant_type = QUANT_TYPE if enable_bias_quant else QuantType.FP
+    return qnn.QuantLinear(in_channels, out_channels,
+                           weight_quant=weight_quant,
+                           bias_quant=bias_quant,
+                           return_quant_tensor=return_quant_tensor,
+                           output_quant=output_quant,
+                           output_bit_width=bit_width,
+                           bias=bias,
+                           bias_quant_type=bias_quant_type,
+                           compute_output_bit_width=bias and enable_bias_quant,
+                           compute_output_scale=bias and enable_bias_quant,
+                           weight_bit_width=bit_width,
+                           weight_quant_type=weight_quant_type,
+                           weight_scaling_impl_type=weight_scaling_impl_type,
+                           weight_scaling_stats_op=weight_scaling_stats_op,
+                           weight_scaling_per_output_channel=weight_scaling_per_output_channel,
+                           weight_restrict_scaling_type=weight_restrict_scaling_type,
+                           weight_narrow_range=weight_narrow_range,
+                           weight_scaling_min_val=weight_scaling_min_val
+                           )
+
+
+def make_quant_relu(bit_width,
+                    quant_type=QUANT_TYPE,
+                    scaling_impl_type=ACT_SCALING_IMPL_TYPE,
+                    scaling_per_channel=ACT_SCALING_PER_CHANNEL,
+                    restrict_scaling_type=ACT_SCALING_RESTRICT_SCALING_TYPE,
+                    scaling_min_val=SCALING_MIN_VAL,
+                    max_val=ACT_MAX_VAL,
+                    return_quant_tensor=ACT_RETURN_QUANT_TENSOR,
+                    per_channel_broadcastable_shape=ACT_PER_CHANNEL_BROADCASTABLE_SHAPE):
+    return qnn.QuantReLU(bit_width=bit_width,
+                         quant_type=quant_type,
+                         scaling_impl_type=scaling_impl_type,
+                         scaling_per_channel=scaling_per_channel,
+                         restrict_scaling_type=restrict_scaling_type,
+                         scaling_min_val=scaling_min_val,
+                         max_val=max_val,
+                         return_quant_tensor=return_quant_tensor,
+                         output_quant=ACT_QUANTIZER,
+                         output_bit_width=bit_width,
+                         per_channel_broadcastable_shape=per_channel_broadcastable_shape
+                         )
