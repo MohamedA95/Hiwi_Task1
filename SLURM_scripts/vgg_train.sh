95a5ad19492396908a771834080e6f94b0fa640a
Mohamed Moursi <mahmedgamal995@gmail.com>
2022-02-21 12:10:16 +0100

    vgg config & train script

--- SLURM_scripts/vgg_train.sh
+++ SLURM_scripts/vgg_train.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
-#SBATCH -J test
+#SBATCH -J devDDP
 #SBATCH -N 1
-#SBATCH -o /home/mmoursi/Hiwi_Task1/res/test.out
-#SBATCH -e /home/mmoursi/Hiwi_Task1/res/test.err
-#SBATCH --gres=gpu:K80:1
-#SBATCH --cpus-per-task=2
-#SBATCH --mem=8192
-#SBATCH --mail-type=END
-#SBATCH --time 2:00:00
+#SBATCH --chdir /home/mmoursi/Hiwi_Task1
+#SBATCH -o res/devDDP_out.log
+#SBATCH -e res/devDDP_err.log
+#SBATCH --exclude=gpu[013,014,015]
+#SBATCH --gres=gpu:V100:4
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=64G
+#SBATCH --mail-type=START,END
+#SBATCH --time=4-00:00:00
 
 echo "Executing on $HOSTNAME"
 date
 module load nvidia/latest
 module load cudnn/latest 
-python3 /home/mmoursi/Hiwi_Task1/train.py -c /home/mmoursi/Hiwi_Task1/config_json/vgg_config_copy.json
+module load gcc/latest
+python3 train_ddp.py -c config_json/vgg_config.json
